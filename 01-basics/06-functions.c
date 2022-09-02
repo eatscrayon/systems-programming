06325b813511f0e2addbf43a7e5c98eeefbbf3fd
eatscrayon <user@computer.boop>
2022-09-02 14:35:32 -0400

    functions

---
+++ 01-basics/06-functions.c
@@ -0,0 +1,76 @@
+/* This file shows examples of defining and calling C functions.
+ * It also demonstrates using scanf().
+ */
+
+#include <stdio.h>
+
+/* This is an example of a FUNCTION PROTOTYPE.  It declares just the type
+ * information for a function (the function's name, return type, and parameter
+ * list). A prototype is used when code in main wants to call the function
+ * before its full definition appears in the file.
+ */
+int max(int n1, int n2);
+
+/* A prototype for another function.  void is the return type of a function
+ * that does not return a value
+ */
+void print_table(int start, int stop);
+
+/* All C programs must have a main function.  This function defines what the
+ * program does when it begins executing, and it's typically used to organize
+ * the big-picture behavior of the program.
+ */
+int main() {
+    int x, y, larger;
+
+    printf("This program will operate over two int values.\n");
+
+    printf("Enter the first value: ");
+    scanf("%d", &x);
+
+    printf("Enter the second value: ");
+    scanf("%d", &y);
+
+    larger = max(x, y);
+
+    printf("The larger of %d and %d is %d\n", x, y, larger);
+
+    print_table(x, larger);
+
+    return 0;
+}
+
+/* This is an example of a FUNCTION DEFINITION.  It specifies not only the
+ * function name and type, but it also fully defines the code of its body.
+ * (Notice, and emulate, the complete function comment!)
+ */
+/* Computes the max of two integer values.
+ *   n1: the first value
+ *   n2: the other value
+ *   returns: the larger of n1 and n2
+ */
+int max(int n1, int n2)  {
+    int result;
+
+    result = n1;
+
+    if (n2 > n1) {
+        result = n2;
+    }
+
+    return result;
+}
+
+/* prints out the squares from start to stop
+ *   start: the beginning of the range
+ *   stop: the end of the range
+ */
+void print_table(int start, int stop) {
+    int i;
+
+    for (i = start; i <= stop; i++) {
+        printf("%d\t", i*i);
+    }
+
+    printf("\n");
+}
