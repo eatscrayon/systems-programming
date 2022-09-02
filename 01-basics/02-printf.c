14803830f86ec6b8442bf651552ebc56938f82b6
eatscrayon <user@computer.boop>
2022-09-02 08:31:19 -0400

    printf

---
+++ 01-basics/02-printf.c
@@ -0,0 +1,12 @@
+/* C printf example */
+#include <stdio.h> // needed for printf
+
+int main() {
+
+    printf("Name: %s \t Info:\n", "Vijay");
+    printf("Age: %d \t Ht: %g\n",20,5.9);
+    printf("Year: %d \t Dorm: %s\n",3,"Alice Paul");
+    printf("ch value is %d which is the ASCII value of  %c\n", 'A', 'A');
+
+    return 0;
+}
